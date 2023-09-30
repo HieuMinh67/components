a0021f2d4710b306ca379883ab11ca251270aff4
Hieu Pham <minhhieu060799@gmail.com>
2023-09-30 16:38:24 +0700

    Edit white list IP

--- platform/bin/variables.sh
+++ platform/bin/variables.sh
@@ -17,6 +17,6 @@ AccountId=$(aws sts get-caller-identity | jq --raw-output ".Account")
 
 BucketName="${ENV}-${AppNameLowercase}-content-${AccountId}"
 BucketResourceName="${BucketName}-s3-bucket"
-WhitelistedIps="44.227.129.192"
+WhitelistedIps="1.54.154.201"
 BucketWebsiteHost="${BucketName}.s3-website-${Region}.amazonaws.com"
 BucketS3Host="${BucketName}.s3.${Region}.amazonaws.com"
